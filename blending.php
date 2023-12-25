fd5a3e9773a8323a4d2412033513bb3304f34dc4
albanobattistella <34811668+albanobattistella@users.noreply.github.com>
2023-12-25 19:01:14 +0100

    Create blending.php

---
+++ blending.php
@@ -0,0 +1,91 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_blending
+ * @category    string
+ * @copyright   2023 Tom Berend  <tom@communityreading.org>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Blending';
+$string['modulename'] = 'Blending';
+$string['pluginadministration'] = 'mod_blending';
+
+$string['blendingname'] = 'Name of Course';
+$string['blendingfieldset'] = 'Settings';
+$string['blendingsettings'] = 'Settings';
+
+$string['displayoptions'] = 'Display Options';
+$string['configdisplayoptions'] = 'Display Options';
+$string['printheading'] = 'Print Heading';
+$string['printheadingexplain'] = 'Print Heading Explain';
+
+$string['studentname'] = 'Student Name';
+$string['tutor1email'] = 'Tutor 1 Email';
+$string['tutor2email'] = 'Tutor 2 Email';
+$string['tutor3email'] = 'Tutor 3 Email';
+
+// navbar
+$string['addstudent'] = 'Add Student';
+$string['exit'] = 'Exit';
+$string['next'] = 'Next';
+$string['navigate'] = 'Navigate';
+$string['about'] = 'About';
+$string['dictionary'] = 'Dictionary';
+$string['lessons'] = 'Lessons';
+
+$string['areyousure'] = 'Are you sure?';
+
+$string['name'] = 'Student';
+$string['lastlesson'] = 'Last Visit';
+$string['lesson'] = 'Lesson';
+$string['history'] = 'History';
+$string['edit'] = 'Edit Tutors';
+$string['delete'] = 'Delete Student';
+
+$string['next'] = 'Next';
+
+$string['refresh'] = 'Refresh';
+$string['start'] = 'Start';
+$string['stop'] = 'Stop';
+$string['reset'] = 'Reset';
+
+$string['introduction'] = 'Introduction';
+$string['lessons'] = 'Lesson';
+$string['mastered'] = 'Mastered';
+$string['inprogress'] = 'In Progress';
+$string['optionalcomment'] = 'Optional Comment...';
+$string['completed'] = 'Completed';
+
+$string['plain'] = 'Plain';
+$string['nonContent'] = 'Non-Content';
+$string['assisted'] = 'Assisted';
+$string['affixes'] = 'Affixes';
+$string['sounds'] = 'Sounds';
+
+$string['finished'] = "Congratulations!  Your student has completed the last lesson in this course!";
+
+// $string[''] = '';
+// $string[''] = '';
+// $string[''] = '';
+// $string[''] = '';
+// $string[''] = '';
+// $string[''] = '';
